48ccc245f724932a312f74ea49447951c508f370
Ian VanSchooten <ian.vanschooten@gmail.com>
2017-07-19 11:55:58 -0400

    Allow getClassName to take an options argument
    The only option is to silence styleName errors, for example in production.
    If `silenceStyleNameErrors` is true, the styleNames which cannot be
    found will simply be removed from the resulting classNames, matching
    the behavior of `react-css-modules`.
    
    This commit alone will not result in a change in behavior, the next
    steps will be to pass in the option from the babel-plugin.

--- src/getClassName.js
+++ src/getClassName.js
@@ -9,7 +9,7 @@ const isNamespacedStyleName = (styleName: string): boolean => {
   return styleName.indexOf('.') !== -1;
 };
 
-const getClassNameForNamespacedStyleName = (styleName: string, styleModuleImportMap: StyleModuleImportMapType): string => {
+const getClassNameForNamespacedStyleName = (styleName: string, styleModuleImportMap: StyleModuleImportMapType, silenceErrors: boolean): ?string => {
   // Note:
   // Do not use the desctructing syntax with Babel.
   // Desctructing adds _slicedToArray helper.
@@ -18,22 +18,39 @@ const getClassNameForNamespacedStyleName = (styleName: string, styleModuleImport
   const moduleName = styleNameParts[1];
 
   if (!moduleName) {
-    throw new Error('Invalid style name.');
+    if (silenceErrors) {
+      return null;
+    } else {
+      throw new Error('Invalid style name.');
+    }
   }
 
   if (!styleModuleImportMap[importName]) {
-    throw new Error('CSS module import does not exist.');
+    if (silenceErrors) {
+      return null;
+    } else {
+      throw new Error('CSS module import does not exist.');
+    }
   }
 
   if (!styleModuleImportMap[importName][moduleName]) {
-    throw new Error('CSS module does not exist.');
+    if (silenceErrors) {
+      return null;
+    } else {
+      throw new Error('CSS module does not exist.');
+    }
   }
 
   return styleModuleImportMap[importName][moduleName];
 };
 
-export default (styleNameValue: string, styleModuleImportMap: StyleModuleImportMapType): string => {
+type OptionsType = {|
+  silenceStyleNameErrors: boolean
+|};
+
+export default (styleNameValue: string, styleModuleImportMap: StyleModuleImportMapType, options?: OptionsType): string => {
   const styleModuleImportMapKeys = Object.keys(styleModuleImportMap);
+  const silenceStyleNameErrors = Boolean(options && options.silenceStyleNameErrors);
 
   return styleNameValue
     .split(' ')
@@ -42,7 +59,7 @@ export default (styleNameValue: string, styleModuleImportMap: StyleModuleImportM
     })
     .map((styleName) => {
       if (isNamespacedStyleName(styleName)) {
-        return getClassNameForNamespacedStyleName(styleName, styleModuleImportMap);
+        return getClassNameForNamespacedStyleName(styleName, styleModuleImportMap, silenceStyleNameErrors);
       }
 
       if (styleModuleImportMapKeys.length === 0) {
@@ -55,11 +72,15 @@ export default (styleNameValue: string, styleModuleImportMap: StyleModuleImportM
 
       const styleModuleMap: StyleModuleMapType = styleModuleImportMap[styleModuleImportMapKeys[0]];
 
-      if (!styleModuleMap[styleName]) {
+      if (!styleModuleMap[styleName] && !silenceStyleNameErrors) {
         throw new Error('Could not resolve the styleName \'' + styleName + '\'.');
       }
 
       return styleModuleMap[styleName];
+    })
+    .filter((className) => {
+      // Remove any styles which could not be found (if silenceStyleNameErrors)
+      return className;
     })
     .join(' ');
 };
