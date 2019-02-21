0363d6098d6c8989c56ee41960789b6a7b13ff5e
AlbertLucianto <albertlucianto@gmail.com>
2019-02-21 13:10:01 +0800

    refactor: move default generateScopedName to optionsDefault

--- src/schemas/optionsDefaults.js
+++ src/schemas/optionsDefaults.js
@@ -2,6 +2,7 @@ const optionsDefaults = {
   attributeNames: {
     styleName: 'className'
   },
+  generateScopedName: '[path]___[name]__[local]___[hash:base64:5]',
   handleMissingStyleName: 'throw'
 };
 
