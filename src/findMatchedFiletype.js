3dd9bfe34b34217dd70388836ad533fa574608d4
super-cattle <xuxinhang4567#126.com>
2019-03-03 23:12:40 +0800

    feat: modify the priority between RegExp pattern filetype and extension style filetype

--- src/findMatchedFiletype.js
+++ src/findMatchedFiletype.js
@@ -1,6 +1,16 @@
 // @flow
 
 export default (sourceFilePath: string, filetypes: $ReadOnlyArray<string>): ?string => {
+  // Try to match as the RegExp pattern
+  for (const pattern of filetypes) {
+    if (pattern.match(/^\.[a-z0-9A-Z]+?$/)) {
+      continue;
+    }
+    if (sourceFilePath.match(new RegExp(pattern))) {
+      return pattern;
+    }
+  }
+
   const extensionDotIndex = sourceFilePath.lastIndexOf('.');
 
   if (extensionDotIndex > -1) {
@@ -9,16 +19,6 @@ export default (sourceFilePath: string, filetypes: $ReadOnlyArray<string>): ?str
 
     if (index > -1) {
       return filetypes[index];
-    }
-  }
-
-  // Try to match as the RegExp pattern
-  for (const pattern of filetypes) {
-    if (pattern.match(/^\.[a-z0-9A-Z]+?$/)) {
-      continue;
-    }
-    if (sourceFilePath.match(new RegExp(pattern))) {
-      return pattern;
     }
   }
 
