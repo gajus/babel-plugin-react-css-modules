40fa1e55a6472afe5b3e7559ee5ceac08fc320c1
AlbertLucianto <albertlucianto@gmail.com>
2019-03-21 15:01:10 +0800

    test: add test case for spread

--- test/fixtures/react-css-modules/handle spread attributes/input.js
+++ test/fixtures/react-css-modules/handle spread attributes/input.js
@@ -8,7 +8,7 @@ const rest = {};
 
 <div {...rest} activeClassName={this.props.activeClassName} activeStyleName="a" styleName="a"></div>;
 
-<div {...rest} activeClassName='apple banana' activeStyleName="a"></div>;
+<div {...rest} activeStyleName="a" activeClassName="b"></div>;
 
 // Should be okay if rest is put on last
 <div styleName="a" {...rest}></div>;
@@ -17,4 +17,6 @@ const rest2 = {};
 
 <div {...rest} {...rest2} styleName="a"></div>;
 
+// Should not do anything
 <div {...rest} {...rest2}></div>;
+<div {...rest} {...rest2} className="b"></div>;
--- test/fixtures/react-css-modules/handle spread attributes/output.js
+++ test/fixtures/react-css-modules/handle spread attributes/output.js
@@ -10,9 +10,11 @@ const rest = {};
 <div {..._objectWithoutProperties(rest, ["styleName", "className", "activeStyleName", "activeClassName"])} className={"b foo__a" + (" " + rest.className)}></div>;
 <div {..._objectWithoutProperties(rest, ["styleName", "className", "activeStyleName", "activeClassName"])} className={"foo__a" + (" " + rest.className)}></div>;
 <div {..._objectWithoutProperties(rest, ["styleName", "className", "activeStyleName", "activeClassName"])} activeClassName={((void 0).props.activeClassName ? (void 0).props.activeClassName + " " : "") + "foo__a" + (" " + rest.activeClassName)} className={"foo__a" + (" " + rest.className)}></div>;
-<div {..._objectWithoutProperties(rest, ["styleName", "className", "activeStyleName", "activeClassName"])} activeClassName={"apple banana foo__a" + (" " + rest.activeClassName)}></div>; // Should be okay if rest is put on last
+<div {..._objectWithoutProperties(rest, ["styleName", "className", "activeStyleName", "activeClassName"])} activeClassName={"b foo__a" + (" " + rest.activeClassName)}></div>; // Should be okay if rest is put on last
 
 <div className={"foo__a" + (" " + rest.className)} {..._objectWithoutProperties(rest, ["styleName", "className", "activeStyleName", "activeClassName"])}></div>;
 const rest2 = {};
-<div {..._objectWithoutProperties(rest, ["styleName", "className", "activeStyleName", "activeClassName"])} {..._objectWithoutProperties(rest2, ["styleName", "className", "activeStyleName", "activeClassName"])} className={"foo__a" + (" " + (rest.className + (" " + rest2.className)))}></div>;
+<div {..._objectWithoutProperties(rest, ["styleName", "className", "activeStyleName", "activeClassName"])} {..._objectWithoutProperties(rest2, ["styleName", "className", "activeStyleName", "activeClassName"])} className={"foo__a" + (" " + (rest.className + (" " + rest2.className)))}></div>; // Should not do anything
+
 <div {...rest} {...rest2}></div>;
+<div {...rest} {...rest2} className="b"></div>;
